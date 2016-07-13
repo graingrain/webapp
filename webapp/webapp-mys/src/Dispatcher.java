50699035eec257d0abf1cbd6299ef557a0bb211b
alain.grainville <alain.grainville@172.31.180.20>
2016-07-13 10:12:16 +0400

    first change

--- webapp/webapp-mys/src/Dispatcher.java
+++ webapp/webapp-mys/src/Dispatcher.java
@@ -51,8 +51,9 @@ public class Dispatcher extends HttpServlet {
     
     public void init(ServletConfig config) throws ServletException {
         super.init(config);
-        System.out.println("-----------git 222222222222222222222222  ------------------------------------");
-        System.out.println("-----------webapp  juin 2016a 64bits  x  ------------------------------------");
+        System.out.println("-----------git dddddddddddd 2222  ------------------------------------");
+        System.out.println("-----------first change  ------------------------------------");
+          System.out.println("-----------webapp  juin 2016a 64bits  x  ------------------------------------");
         System.out.println("initserveur:"+InitServeur.dbgInitParameter(this));
         Const.dbg = InitServeur.valeurDebug(this);
         System.out.println("CneServlet.init Const.dbg="+Const.dbg);
